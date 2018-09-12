353f4edf4a157d27bf2378935a2bf642b18a4015
Andreas Remdt <andreas.remdt@gmail.com>
2018-09-12 16:38:44 +0200

    add gulp asset pipeline

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var babel = require("gulp-babel");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+
+gulp.task("scss", function () {
+  return gulp.src("assets/src/scss/styles.scss")
+    .pipe(sass({ outputStyle: "compressed" })
+      .on("error", sass.logError))
+    .pipe(autoprefixer({ browsers: ["last 2 versions"], grid: true }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("assets/css"));
+});
+
+gulp.task("js", function () {
+  return gulp.src("assets/src/js/*.js")
+    .pipe(babel({ presets: ["@babel/env"] }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("assets/js"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch("assets/src/scss/**/*.scss", ["scss"]);
+  gulp.watch("assets/src/js/*.js", ["js"]);
+});
+
+gulp.task("default", ["scss", "js"]);
